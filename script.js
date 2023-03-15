ad35066069509b9834719403d2707f55fd0e9edf
aaronkossam <aaronkossam23@gmail.com>
2023-03-15 12:42:13 +0000

    add client side validation

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const email =document.getElementById('email-1');
+const form =document.getElementById('form');
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    checkInputs()
+})
+
+function checkInputs(){
+    const emailValue =email.value.trim()
+}
+
+if(emailValue=''){
+    SetErrorfor(emailValue,'email cannot be empty')
+}
+
 
 
 
